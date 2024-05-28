f6efc13f158b8967fb282d7683f4d543e4b631d1
Minnthu <minnthu.026@gmail.com>
2024-05-28 08:55:37 +0630

    specified todo

--- src/App.jsx
+++ src/App.jsx
@@ -2,40 +2,52 @@ import { useEffect, useState } from "react";
 
 
 function App() {
-  const [todos,setTodos] = useState([]);
+  const [id,setId] = useState('');
+  const [error,setError] = useState(false);
+  const [todo,setTodo] = useState('');
 
-  useEffect((_) => {
-    fetchData();
-  },[]);
+  const getData = async (e) => {
+    e.preventDefault();
+    // console.log('getting data');
 
-  const fetchData = async ()=>{
-    const response = await fetch("https://jsonplaceholder.typicode.com/todos");
+
+    if(id < 1){
+      setError(true);
+      setId('');
+      return;
+    }
+
+    const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`);
     const data = await response.json();
-    setTodos(data);
+    console.log(data);
+    setId('');
+    setTodo(data);
+    setError(false);
   }
 
   return (
     <section>
-      <table>
-        <thead>
-          <tr>
-            <th>id</th>
-            <th>title</th>
-            <th>completed</th>
-          </tr>
-        </thead>
-        <tbody>
-          {todos.map((todo) => (
-            <tr key={todo.id}>
-              <td>{todo.id}</td>
-              <td>{todo.title}</td>
-              {
-                todo.completed ? (<><td className="done">Done</td></>) : (<><td className="none">None</td></>)
-              }
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <form onSubmit={getData}>
+        <input
+          type="number"
+          value={id}
+          onChange={(e) => {
+            setId(e.target.value);
+          }}
+        />
+        <button type="submit">Get Data</button>
+      </form>
+      <div>
+        {error && <h1>Please enter an valid Id. Example (1,2,3,...)</h1>}
+        {todo && (
+          <div>
+            <h1>Id - {todo.id}</h1>
+            <h1>Title - {todo.title}</h1>
+            <h1>UserId - {todo.userId}</h1>
+            <h1>Completed - {todo.completed ? (<span>True</span>) : (<span>False</span>)}</h1>
+          </div>
+        )}
+      </div>
     </section>
   );
 
